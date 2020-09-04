66e99890350c75268d3bc6db43de1cff30ff7fa0
ouwou <26526779+ouwou@users.noreply.github.com>
2020-09-04 01:03:43 -0400

    add a very important feature

--- abaddon.cpp
+++ abaddon.cpp
@@ -207,6 +207,8 @@ void Abaddon::ActionChatLoadHistory(Snowflake id) {
 }
 
 void Abaddon::ActionChatInputSubmit(std::string msg, Snowflake channel) {
+    if (msg.substr(0, 7) == "/shrug " || msg == "/shrug")
+        msg = msg.substr(6) + "\xC2\xAF\x5C\x5F\x28\xE3\x83\x84\x29\x5F\x2F\xC2\xAF"; // this is important
     m_discord.SendChatMessage(msg, channel);
 }
 
