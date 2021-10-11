bf560ae9d25f91ab353c7be4656875f4fda791dd
ouwou <26526779+ouwou@users.noreply.github.com>
2021-10-11 01:03:31 -0400

    use find modules for ixwebsocket/simpleini

--- cmake/Findixwebsocket.cmake
+++ cmake/Findixwebsocket.cmake
@@ -1,16 +1,30 @@
-find_path(IXWEBSOCKET_INCLUDE_DIR
-		  NAMES ixwebsocket/IXWebSocket.h)
+set(IXWebSocket_LIBRARY_NAME ixwebsocket)
 
-find_library(IXWEBSOCKET_LIBRARY
-		     NAMES ixwebsocket
-		     HINTS ${IXWEBSOCKET_LIBRARY_ROOT})
+find_path(IXWebSocket_INCLUDE_DIR
+          NAMES ixwebsocket/IXWebSocket.h
+          HINTS /usr/include
+                /usr/local/include
+                /opt/local/include
+          PATH_SUFFIXES ${IXWebSocket_LIBRARY_NAME})
 
-set(IXWEBSOCKET_LIBRARIES    ${IXWEBSOCKET_LIBRARY})
-set(IXWEBSOCKET_INCLUDE_DIRS ${IXWEBSOCKET_INCLUDE_DIR})
+
+find_library(IXWebSocket_LIBRARY
+             NAMES ${IXWebSocket_LIBRARY_NAME}
+             PATH_SUFFIXES ${IXWebSocket_LIBRARY_NAME}
+                           ${IXWebSocket_LIBRARY_NAME}/include)
+
+
+find_package(OpenSSL QUIET)
+set(IXWebSocket_INCLUDE_DIRS "${IXWebSocket_INCLUDE_DIR};${OPENSSL_INCLUDE_DIR}")
+set(IXWebSocket_LIBRARIES "${IXWebSocket_LIBRARY};${OPENSSL_LIBRARIES}")
+
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ixwebsocket
+
+find_package_handle_standard_args(IXWebSocket
                                   REQUIRED_VARS
-                                    IXWEBSOCKET_LIBRARY
-                                    IXWEBSOCKET_INCLUDE_DIR
-                                  VERSION_VAR IXWEBSOCKET_VERSION)
+                                    IXWebSocket_LIBRARY
+                                    IXWebSocket_INCLUDE_DIR)
+
+
+mark_as_advanced(IXWebSocket_LIBRARY IXWebSocket_INCLUDE_DIR)
