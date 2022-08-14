9e2c0815faa84c7e43d472923cb2fade35cbbfea
dragontamer8740 <dragontamer8740@gmail.com>
2022-08-13 22:36:21 -0400

    clang-format i guess

--- src/emojis.cpp
+++ src/emojis.cpp
@@ -5,17 +5,17 @@
 
 #ifdef ABADDON_IS_BIG_ENDIAN
 /* Allows processing emojis.bin correctly on big-endian systems. */
-int emojis_int32_correct_endian (int little_endian_in) {
-  /* this does the same thing as __bswap_32() but can be done without
-     non-standard headers. */
-  return ((little_endian_in >> 24) & 0xff) |      // move byte 3 to byte 0
-         ((little_endian_in <<  8) & 0xff0000) |  // move byte 1 to byte 2
-         ((little_endian_in >>  8) & 0xff00) |    // move byte 2 to byte 1
-         ((little_endian_in << 24) & 0xff000000); // byte 0 to byte 3
+int emojis_int32_correct_endian(int little_endian_in) {
+    /* this does the same thing as __bswap_32() but can be done without
+       non-standard headers. */
+    return ((little_endian_in >> 24) & 0xff) |      // move byte 3 to byte 0
+           ((little_endian_in << 8) & 0xff0000) |   // move byte 1 to byte 2
+           ((little_endian_in >> 8) & 0xff00) |     // move byte 2 to byte 1
+           ((little_endian_in << 24) & 0xff000000); // byte 0 to byte 3
 }
 #else
 int emojis_int32_correct_endian(int little_endian_in) {
-  return little_endian_in;
+    return little_endian_in;
 }
 #endif
 
