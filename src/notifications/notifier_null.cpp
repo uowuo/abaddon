3889991e16958d9c25bcc982a70aa232b3c16e9a
ouwou <26526779+ouwou@users.noreply.github.com>
2023-03-10 03:11:03 -0500

    fix null notifier again maybe

--- src/notifications/notifier_null.cpp
+++ src/notifications/notifier_null.cpp
@@ -4,4 +4,4 @@ Notifier::Notifier() {}
 
 Notifier::~Notifier() {}
 
-void Notifier::Notify(const Glib::ustring &title, const Glib::ustring &text, const Glib::ustring &default_action) {}
+void Notifier::Notify(const Glib::ustring &title, const Glib::ustring &text, const Glib::ustring &default_action, const std::string &icon_path) {}
