5d3e5eec5e32c49247a0a1fbae1817a4b183f6cd
ouwou <26526779+ouwou@users.noreply.github.com>
2023-05-12 20:28:44 -0400

    try to fix voice connect signal again

--- src/discord/discord.cpp
+++ src/discord/discord.cpp
@@ -2270,8 +2270,10 @@ void DiscordClient::HandleGatewayVoiceStateUpdate(const GatewayMessage &msg) {
         SetVoiceState(data.UserID, *data.ChannelID);
         if (old_state.has_value() && *old_state != *data.ChannelID) {
             m_signal_voice_user_disconnect.emit(data.UserID, *old_state);
+            m_signal_voice_user_connect.emit(data.UserID, *data.ChannelID);
+        } else if (!old_state.has_value()) {
+            m_signal_voice_user_connect.emit(data.UserID, *data.ChannelID);
         }
-        m_signal_voice_user_connect.emit(data.UserID, *data.ChannelID);
     } else {
         const auto old_state = GetVoiceState(data.UserID);
         ClearVoiceState(data.UserID);
