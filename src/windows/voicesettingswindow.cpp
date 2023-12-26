155d95e29cc50a66cbe7711b172b195be637bc48
ouwou <26526779+ouwou@users.noreply.github.com>
2023-12-26 04:19:19 -0500

    fix crash when closing VoiceSettingsWindow

--- src/windows/voicesettingswindow.cpp
+++ src/windows/voicesettingswindow.cpp
@@ -112,9 +112,12 @@ VoiceSettingsWindow::VoiceSettingsWindow()
     m_gain.set_increments(1.0, 5.0);
     m_gain.set_range(0.0, 6969696969.0);
     m_gain.set_value(Abaddon::Get().GetAudio().GetCaptureGain() * 100.0);
-    m_gain.signal_value_changed().connect([this]() {
+    const auto cb = [this]() {
+        spdlog::get("ui")->warn("emit");
         m_signal_gain.emit(m_gain.get_value() / 100.0);
-    });
+    };
+    // m_gain.signal_value_changed can be fired during destruction. thankfully signals are trackable
+    m_gain.signal_value_changed().connect(sigc::track_obj(cb, *this, m_signal_gain));
 
     auto *layout = Gtk::make_managed<Gtk::HBox>();
     auto *labels = Gtk::make_managed<Gtk::VBox>();
