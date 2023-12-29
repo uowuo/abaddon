d2ff7112aac4f65106b99a92fb5f3492e7e5a827
ouwou <26526779+ouwou@users.noreply.github.com>
2023-12-29 02:06:54 -0500

    add notification sounds only to windows

---
+++ src/notifications/notifier_fallback.cpp
@@ -0,0 +1,30 @@
+#include "notifier.hpp"
+
+/* no actual notifications, just sounds
+  GNotification has no win32 backend, and WinToast uses headers msys2 doesnt provide
+  maybe it can be LoadLibrary'd in :s
+*/
+
+Notifier::Notifier() {
+#ifdef ENABLE_NOTIFICATION_SOUNDS
+    if (ma_engine_init(nullptr, &m_engine) != MA_SUCCESS) {
+        printf("failed to initialize miniaudio engine\n");
+    }
+#endif
+}
+
+Notifier::~Notifier() {
+#ifdef ENABLE_NOTIFICATION_SOUNDS
+    ma_engine_uninit(&m_engine);
+#endif
+}
+
+void Notifier::Notify(const Glib::ustring &id, const Glib::ustring &title, const Glib::ustring &text, const Glib::ustring &default_action, const std::string &icon_path) {
+#ifdef ENABLE_NOTIFICATION_SOUNDS
+    if (Abaddon::Get().GetSettings().NotificationsPlaySound) {
+        ma_engine_play_sound(&m_engine, Abaddon::Get().GetResPath("/sound/message.mp3").c_str(), nullptr);
+    }
+#endif
+}
+
+void Notifier::Withdraw(const Glib::ustring &id) {}
--- src/notifications/notifier_null.cpp
+++
@@ -1,9 +0,0 @@
-#include "notifier.hpp"
-
-Notifier::Notifier() {}
-
-Notifier::~Notifier() {}
-
-void Notifier::Notify(const Glib::ustring &id, const Glib::ustring &title, const Glib::ustring &text, const Glib::ustring &default_action, const std::string &icon_path) {}
-
-void Notifier::Withdraw(const Glib::ustring &id) {}
