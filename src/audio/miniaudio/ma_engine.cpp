3d89d24a43016f3d311bb5e57361050c16d109a4
Ryze312 <50497128+ryze312@users.noreply.github.com>
2024-06-02 20:14:15 +0300

    Wrap MaEngine

---
+++ src/audio/miniaudio/ma_engine.cpp
@@ -0,0 +1,54 @@
+#include "ma_engine.hpp"
+
+namespace AbaddonClient::Audio::Miniaudio {
+
+MaEngine::MaEngine(EnginePtr &&engine) noexcept :
+    m_engine(std::move(engine)) {}
+
+std::optional<MaEngine> MaEngine::Create(ma_engine_config &&config) noexcept {
+    EnginePtr engine = EnginePtr(new ma_engine);
+
+    const auto result = ma_engine_init(&config, engine.get());
+    if (result != MA_SUCCESS) {
+        spdlog::get("audio")->error("Failed to create engine: {}", ma_result_description(result));
+        return std::nullopt;
+    }
+
+    return MaEngine(std::move(engine));
+}
+
+bool MaEngine::Start() noexcept {
+    const auto result = ma_engine_start(m_engine.get());
+    if (result != MA_SUCCESS) {
+        spdlog::get("audio")->error("Failed to start engine: {}", ma_result_description(result));
+        return false;
+    }
+
+    return true;
+}
+
+bool MaEngine::Stop() noexcept {
+    const auto result = ma_engine_stop(m_engine.get());
+    if (result != MA_SUCCESS) {
+        spdlog::get("audio")->error("Failed to stop engine: {}", ma_result_description(result));
+        return false;
+    }
+
+    return true;
+}
+
+bool MaEngine::PlaySound(std::string_view file_path) noexcept {
+    const auto result = ma_engine_play_sound(m_engine.get(), file_path.data(), nullptr);
+    if (result != MA_SUCCESS) {
+        spdlog::get("audio")->error("Failed to play sound at {}: {}", file_path.data(), ma_result_description(result));
+        return false;
+    }
+
+    return true;
+}
+
+ma_engine& MaEngine::GetInternal() noexcept {
+    return *m_engine;
+}
+
+}
---
+++ src/audio/miniaudio/ma_engine.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <miniaudio.h>
+
+namespace AbaddonClient::Audio::Miniaudio {
+
+class MaEngine {
+public:
+    static std::optional<MaEngine> Create(ma_engine_config &&config) noexcept;
+
+    bool Start() noexcept;
+    bool Stop() noexcept;
+
+    bool PlaySound(std::string_view file_path) noexcept;
+
+    ma_engine& GetInternal() noexcept;
+
+private:
+    struct EngineDeleter {
+        void operator()(ma_engine* ptr) noexcept {
+            ma_engine_uninit(ptr);
+        }
+    };
+
+    // Put ma_engine behind pointer to allow moving.
+    // miniaudio expects ma_engine reference to be valid at all times
+    // Moving it to other location would cause memory corruption
+    using EnginePtr = std::unique_ptr<ma_engine, EngineDeleter>;
+    MaEngine(EnginePtr &&engine) noexcept;
+
+    EnginePtr m_engine;
+};
+
+}
